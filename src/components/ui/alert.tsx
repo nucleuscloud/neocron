b54550d90b20def7ee5e4770dea6026581c4b1f3
Evis Drenova <80707987+evisdrenova@users.noreply.github.com>
2023-09-22 15:30:19 -0700

    remove css variables and go back to default tailwind utilities (#18)
    * remove css variables and go back to default tailwind utilities
    
    * prettier

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -4,13 +4,13 @@ import * as React from 'react';
 import { cn } from '../../lib/utils';
 
 const alertVariants = cva(
-  'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
+  'relative w-full rounded-lg border border-slate-200 p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-slate-950 dark:border-slate-800 dark:[&>svg]:text-slate-50',
   {
     variants: {
       variant: {
-        default: 'bg-background text-foreground',
+        default: 'bg-white text-slate-950 dark:bg-slate-950 dark:text-slate-50',
         destructive:
-          'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+          'border-red-500/50 text-red-500 dark:border-red-500 [&>svg]:text-red-500 dark:border-red-900/50 dark:text-red-900 dark:dark:border-red-900 dark:[&>svg]:text-red-900',
       },
     },
     defaultVariants: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,5 +16,6 @@ const config: Config = {
     },
   },
   plugins: [],
+  important: true,
 };
 export default config;
